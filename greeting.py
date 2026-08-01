157e19c90b5dff9da6676d2876378e60e4f78d72
kandycediep <89169423+kandycediep@users.noreply.github.com>
2026-08-01 12:54:53 -0700

    Create greeting.py

---
+++ greeting.py
@@ -0,0 +1,9 @@
+def welcome_message():
+    """
+    Returns a welcome message for new interns.
+    """
+    return "Welcom to the team!"
+
+
+if __name__ == "__main__":
+    print(welcome_message())
